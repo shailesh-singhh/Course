d956217621a3b6392f75f2e26b8b6305b7d2374c
Shailesh singh <singhshailesh2901@gmail.com>
2024-11-26 07:37:50 +0530

    Stocks problems

---
+++ BuyAndSell.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class BuyAndSell {
+    public static int  Stock(int price[]){
+  int buyPrice = Integer.MAX_VALUE;
+  int maxProfit = 0;
+  for(int i = 0 ; i<price.length; i++){
+    if(buyPrice<price[i]){  //PROFIT CASE 
+        int profit = price[i]-buyPrice;
+        maxProfit =Math.max(maxProfit,profit);   
+    }
+    else{
+        buyPrice=price[i];
+    }
+  }
+  return maxProfit;
+ }
+    public static void main(String args[]){
+        int price[]={7,1,5,3,6,4};
+       System.out.println(Stock(price)) ;
+}
+}
