80e641e1bd2f3fbb3779633ffcc64a147878302c
Shailesh singh <singhshailesh2901@gmail.com>
2024-12-06 15:05:52 +0530

    Assignment related to 2 d arrays

---
+++ Assignment4.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Assignment4 {
+    public static void CountSevens(int arr[][]){
+        int count = 0;
+        for(int i = 0 ; i< arr.length; i++){
+            for(int j = 0 ; j<arr[0].length;j++){
+                if(arr[i][j]==7){
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+    public static void Sum(int arr[][]){
+        int ad = 0;
+        for(int i = 0 ; i< arr.length ; i++){
+            for(int j = 0 ; j<arr[0].length ; j++){
+                if(i != 0 && i != arr.length-1){
+                    ad = ad +arr[i][j];
+                }
+            }
+        }
+        System.out.println(ad);
+    }
+    public static void Transpose(int arr[][]){
+        
+    }
+    public static void main(String args[]){
+            int arr[][]={{1,4,9},{11,4,3}};
+            // CountSevens(arr);
+            Sum(arr);
+    }
+}
---
+++ first.py
@@ -0,0 +1,1 @@
+print("Hello Wedang and Sonali")
